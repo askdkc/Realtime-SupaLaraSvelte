e18885dbbac63aeab4fec66e4faa38fc90a3603e
askdkc <askdkc@users.noreply.github.com>
2023-08-08 11:01:48 +0900

    Dashboadにステータス変更ボタンとSvelteコンポーネント追加

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,33 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
+        <h2 class="flex justify-around md:justify-center font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            @if(auth()->user()->status == 'free')
+                <button disabled class="mx-3">フリー</button>
+            @else
+                <a href="{{ route('setfree') }}">
+                    <x-primary-button class="mx-3">
+                        フリー
+                    </x-primary-button>
+                </a>
+            @endif
+            @if(auth()->user()->status == 'busy')
+                <button disabled class="mx-3">対応中</button>
+            @else
+                <a href="{{ route('setbusy') }}">
+                    <x-primary-button class="mx-3">
+                        対応中
+                    </x-primary-button>
+                </a>
+            @endif
+            @if(auth()->user()->status == 'vip')
+                <button disabled class="mx-3">VIP</button>
+            @else
+                <a href="{{ route('setvip') }}">
+                    <x-primary-button class="mx-3">
+                        {{ __('VIP') }}
+                    </x-primary-button>
+                </a>
+            @endif
         </h2>
     </x-slot>
 
@@ -9,7 +35,8 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    <div id="app"></div>
+                    @vite(['resources/js/main.js'])
                 </div>
             </div>
         </div>
